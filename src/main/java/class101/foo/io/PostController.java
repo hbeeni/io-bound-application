29e686d0c636ca848e21ea924ff74149f00bcbea
Hyebeen <hyebeen.dev@gmail.com>
2023-12-10 16:31:29 +0900

    feat: db에서 es를 사용하도록 변경

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -2,11 +2,7 @@ package class101.foo.io;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +17,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 public class PostController {
 
-	private static Integer PAGE_SIZE = 20;
-
 	private final PostRepository postRepository;
-	private final PostCacheService postCacheService;
 	private final Producer producer;
 	private final ObjectMapper objectMapper;
 
@@ -36,27 +29,9 @@ public class PostController {
 		return post;
 	}
 
-	// 2. 글 목록을 페이징하여 반환
-	@GetMapping("/posts")
-	public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-		if (page.equals(1)) {
-			return postCacheService.getFirstPostPage();
-		}
-
-		return postRepository.findAll(
-			PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-		);
-	}
-
-	// 3. 글 번호로 조회
-	@GetMapping("/post/{id}")
-	public Post getPostById(@PathVariable Long id) {
-		return postRepository.findById(id).get();
-	}
-
 	// 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 	@GetMapping("/search")
 	public List<Post> findPostsByContent(@RequestParam String content) {
-		return postRepository.findByContentContains(content);
+		return postRepository.findByContent(content);
 	}
 }
